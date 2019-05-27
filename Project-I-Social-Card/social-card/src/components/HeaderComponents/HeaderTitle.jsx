3b862cded137b23ff37d0fb3f1cf5c5c2a68d32c
EmilyAbrahart <emilyabrahart@gmail.com>
2019-05-27 23:54:58 +0100

    Add moment.js library to configure date format

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.jsx
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.jsx
@@ -1,8 +1,10 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
 import './Header.css';
+import moment from 'moment';
 
-function HeaderTitle({ name, handle, date }) {
+function HeaderTitle({ name, handle }) {
+  const date = moment().format('Do MMMM');
   return (
     <div className="headerTitle">
       <div className="name">{name}</div>
